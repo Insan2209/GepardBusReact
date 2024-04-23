90e0738543d204cc513aa3d96241dbc9f1275977
Insan2209 <michalwojtal5@wp.pl>
2024-04-23 20:39:54 +0200

    Started doing "news" site

--- gepard_website/src/pages/News.js
+++ gepard_website/src/pages/News.js
@@ -3,19 +3,27 @@ import React from "react";
 function News () {
     return (
     <>
-        <div className="flex flex-wrap justify-center mt-12 min-h-[85vw] xl:min-h-[25vw]">
-            <div className="w-full xl:w-1/3 flex flex-wrap justify-center bg-red-300">
-                <p className="text-lg md:text-2xl font-semibold text-zinc-700 font-poppins">Bilety miesięczne na miesiąc kwiecień 2024</p>
-                <p className="text-md md:text-xl text-zinc-700 ml-5">Sprzedaż biletów miesięcznych odbędzie się w Częstochowie w budynku dworca PKP, lokal 2.06 w dniach:</p>
-                <ul className="list-disc">
-                    <li>27.03.2024 (środa) w godzinach 7:20 - 17:00</li>
-                    <li>28.03.2024 (czwartek) w godzinach 7:20 - 16:00</li>
-                    <li>02.04.2024 (wtorek) w godzinach 7:20 - 15:00</li>
-                    <li>03.04.2024 (środa) w godzinach 7:20 - 15:00</li>
-                </ul>
+        <div className="flex flex-wrap flex-col space-y-6 justify-start mt-8 md:mt-16 min-h-[85vw] xl:min-h-[25vw] px-8">
+            <div className="w-full md:w-1/3">
+                <div className="flex flex-row items-center w-full justify-between">
+                    <p className="text-2xl md:text-4xl font-bold text-zinc-700 font-poppins text-center">Bilety miesięczne</p>
+                    <img src="/png/down_arrow.svg" alt="down arrow" className="ml-4 h-10"></img>
+                </div>
+                <hr class="h-px w-full md:w-96 my-2 bg-selective_yellow border-0 block"></hr>
             </div>
-            <div className="w-full xl:w-2/3 flex text-center justify-center bg-blue-300">
-
+            <div className="w-full md:w-1/3">
+                <div className="flex flex-row items-center w-full justify-between">
+                    <p className="text-2xl md:text-4xl font-bold text-zinc-700 font-poppins text-center">Aktualności</p>
+                    <img src="/png/down_arrow.svg" alt="down arrow" className="ml-4 h-10"></img>
+                </div>
+                <hr class="h-px w-full md:w-96 my-2 bg-selective_yellow border-0 block"></hr>
+            </div>
+            <div className="w-full md:w-1/3">
+                <div className="flex flex-row items-center w-full justify-between">
+                    <p className="text-2xl md:text-4xl font-bold text-zinc-700 font-poppins text-center">Grupy zorganizowane</p>
+                    <img src="/png/down_arrow.svg" alt="down arrow" className="ml-4 h-10"></img>
+                </div>
+                <hr class="h-px w-full md:w-96 my-2 bg-selective_yellow border-0 block"></hr>
             </div>
         </div>
     </>
