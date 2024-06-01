a4c5de49e4f4aaf5390bc08de72d60dd0854869c
Insan2209 <michalwojtal5@wp.pl>
2024-06-01 19:36:07 +0200

    test

--- gepard_website/src/pages/Schedule.js
+++ gepard_website/src/pages/Schedule.js
@@ -1,13 +1,45 @@
-import React from "react";
+import React from 'react';
+import { useParams } from 'react-router-dom';
 
-function Schedule () {
+const timetableData = {
+    'pajeczno-czestochowa': {
+        0: ['8:00', '12:00', '16:00'],
+        1: ['9:00', '13:00', '17:00'],
+        2: ['10:00', '14:00', '18:00'],
+       
+    },
+    'popow-klobuck': {
+        0: ['6:00', '10:00', '14:00'],
+        1: ['7:00', '11:00', '15:00'],
+        2: ['8:00', '12:00', '16:00'],
+        
+    },
+    'popow-wiecki': {
+        0: ['5:00', '9:00', '13:00'],
+        1: ['6:00', '10:00', '14:00'],
+        2: ['7:00', '11:00', '15:00'],
+        
+    },
+};
+
+function Schedule() {
+    const { routeId, stopIndex } = useParams();
+    const times = timetableData[routeId]?.[stopIndex] || [];
+
     return (
-    <>
-        <div className="flex flex-wrap justify-center my-12 md:mt-24 min-h-[85vw] xl:min-h-[25vw]">
-            
+        <div className="flex flex-wrap justify-center my-12">
+            <div className="w-full xl:w-1/2 block space-y-10 text-center justify-center">
+                <p className="text-4xl md:text-6xl font-bold text-zinc-700 font-poppins mb-16">
+                    Godziny odjazdów:
+                </p>
+                {times.map((time, index) => (
+                    <div key={index} className="text-2xl md:text-4xl font-bold text-zinc-700 font-poppins">
+                        {time}
+                    </div>
+                ))}
+            </div>
         </div>
-    </>
     );
 }
 
-export default Schedule; 
+export default Schedule;
