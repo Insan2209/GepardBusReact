a7792c6371448c508a35f1a947f0e7a0c8db8487
Insan2209 <michalwojtal5@wp.pl>
2024-07-26 21:42:02 +0200

    database updated

--- gepard_website/src/database/init_database.js
+++ gepard_website/src/database/init_database.js
@@ -1,6 +1,5 @@
-const sqlite3 = require('sqlite3').verbose();
+import sqlite3 from 'sqlite3';
 
-// Połącz się z bazą danych
 let db = new sqlite3.Database('./mydatabase.db', sqlite3.OPEN_READWRITE, (err) => {
   if (err) {
     console.error(err.message);
@@ -8,51 +7,31 @@ let db = new sqlite3.Database('./mydatabase.db', sqlite3.OPEN_READWRITE, (err) =
   console.log('Połączono z bazą danych.');
 });
 
-// Definicje godzin odjazdów i odpowiadających im kodów
-const departures = [
-  { time: '05:50', code: 'Dmq' },
-  { time: '06:21', code: 'Dmq' },
-  { time: '06:58', code: 'Demq' },
-  { time: '07:48', code: 'EmqQ' },
-  { time: '08:51', code: 'E7*' },
-  { time: '09:33', code: 'Dmq' },
-  { time: '11:10', code: 'Demq' },
-  { time: '12:17', code: 'Dmq' },
-  { time: '13:40', code: 'Dmq' },
-  { time: '14:10', code: 'E7*' },
-  { time: '15:10', code: 'Dmq' },
-  { time: '15:40', code: '67*' },
-  { time: '17:20', code: 'Dmq' },
-  { time: '18:10', code: 'Demq' },
-  { time: '19:16', code: 'E7*' },
-  { time: '20:23', code: 'Dmq' }
+// Definicje godzin odjazdów i odpowiadających im kodów dla drugiej trasy
+const newDepartures = [
+  { time: '06:24', code: 'D' },
+  { time: '11:34', code: 'D' },
 ];
 
-// Przystanki i czasy przejazdów
+// Przystanki dla drugiej trasy
 const stops = [
-  { id: 1, name: 'Pajęczno Plac Targowy' },
-  { id: 2, name: 'Pajęczno ul. Wieluńska' },
-  { id: 3, name: 'Dylów Rządowy' },
-  { id: 4, name: 'Trębaczew (Kościół)' },
-  { id: 5, name: 'Posmykowizna' },
-  { id: 6, name: 'Działoszyn ul. Piłsudskiego (osiedle)' },
-  { id: 7, name: 'Działoszyn Plac Wolności' },
-  { id: 8, name: 'Raciszyn' },
-  { id: 9, name: 'Smolarze' },
-  { id: 10, name: 'Dębie' },
-  { id: 11, name: 'Popów (Kościół)' },
-  { id: 12, name: 'Zawady' },
-  { id: 13, name: 'Izbiska Duże' },
-  { id: 14, name: 'Miedźno' },
-  { id: 15, name: 'Kołaczkowice' },
-  { id: 16, name: 'Łobodno' },
-  { id: 17, name: 'Kamyk' },
-  { id: 18, name: 'Biała' },
-  { id: 19, name: 'Częstochowa- Real' },
-  { id: 20, name: 'Częstochowa- Promenada Niemena' },
-  { id: 21, name: 'Częstochowa- Hala Polonia' },
-  { id: 22, name: 'Częstochowa II Aleja NMP' },
-  { id: 23, name: 'Częstochowa ul. Orzechowskiego' }
+  { id: 52, name: 'Nowa Wieś k.Brzózek' },
+  { id: 53, name: 'Kłobuck Rynek im.Jana Pawła II' },
+  { id: 54, name: 'Kłobuck ul.11 Listopada/Sagan' },
+  { id: 55, name: 'Kłobuck ul.Zamkowa "Zagórze"' },
+  { id: 56, name: 'Mokra III' },
+  { id: 57, name: 'Wilkowiecko VII ul.Mikołaja z Wilkowiecka' },
+  { id: 58, name: 'Wilkowiecko VI ul.Mikołaja z Wilkowiecka' },
+  { id: 59, name: 'Wilkowiecko XI ul.3 Maja' },
+  { id: 60, name: 'Wilkowiecko X ul.3 Maja' },
+  { id: 61, name: 'Popów Rondo 2' },
+  { id: 62, name: 'Rębielice Królewskie 3' },
+  { id: 63, name: 'Rębielice Królewskie 2' },
+  { id: 64, name: 'Rębielice Królewskie 1 ZOZ' },
+  { id: 65, name: 'Rębielice Królewskie' },
+  { id: 66, name: 'Kamieńszczyzna' },
+  { id: 67, name: 'Zawady 3' },
+  { id: 68, name: 'Zawady 2' },
 ];
 
 // Funkcja obliczająca godziny odjazdów dla wszystkich przystanków
@@ -67,10 +46,10 @@ const calculateDepartureTimes = (initialTime) => {
 
 // Wstawianie danych do tabeli schedules
 db.serialize(() => {
-  departures.forEach((departure) => {
+  newDepartures.forEach((departure) => {
     let times = calculateDepartureTimes(departure.time);
     times.forEach((time) => {
-      let sql = `INSERT INTO schedules (route_id, stop_id, departure_time, schedule_code) VALUES (1, ?, ?, ?)`;
+      let sql = `INSERT INTO schedules (route_id, stop_id, departure_time, schedule_code) VALUES (4, ?, ?, ?)`;
       db.run(sql, [time.stop_id, time.departure_time, departure.code], (err) => {
         if (err) {
           return console.error(err.message);
